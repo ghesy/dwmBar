f8919cbef5d35091da2cc813b9b9cf55c3c847d4
ehsan gn <ehsangn@protonmail.ch>
2021-05-26 22:57:45 +0430

    update comment block of topmodule vector

--- config.hpp
+++ config.hpp
@@ -29,14 +29,12 @@ static const std::string botTopDelimiter(";");
 
 /*
   list of modules
-
 . module-name: is name or path to a script or program.
     can start with ~/... for stuff in your home directory.
-. internal/external: use internal if you want to use
 . refresh-interval: in seconds, 0 means only update on receiving a real-time signal
 . signal-id: SIGRTMIN signal id; must be between 0 and 30.*/
 
-/*  module-name       refresh-interval   signal-id */
+/* module-name, refresh-interval, signal-id */
 static const std::vector< std::vector<std::string> > topModuleList = {
     {"dwmbar-net",       "8",   "4"},
     {"dwmbar-tor",       "9",   "3"},
